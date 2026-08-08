15f17a71f44dc4788bf1a4ddc6dd6b54c5c5aa19
falafe1y <falafelyy@ya.ru>
2026-08-08 23:38:45 +0300

    Update install script

--- install.sh
+++ install.sh
@@ -2,19 +2,11 @@
 
 if [ -d build ];
 then
-	echo "Deleting build directory..."
-	rm -rf build
-	echo "Directory build was deleted"	
-
-	mkdir build
 	cd build
-	cmake ..
-#	 make VERBOSE=1
 	cmake --build .
 else
     mkdir build
     cd build
     cmake ..
-#    make VERBOSE=1
     cmake --build .
 fi
